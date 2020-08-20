1741ff7e74857362d8636f1636e4bee88d19d8d2
haziq <haziq.othman99@gmail.com>
2020-08-20 14:10:39 +0800

    Client API

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log("I HAVE A TOKEN ", token);
+  }, []);
+
+  return (
+    <div className="App">
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+            <Login />
+          )
+      }
+
+    </div>
+  );
+}
+
+export default App;
